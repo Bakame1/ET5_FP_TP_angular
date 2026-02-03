62a6aa37a77a17407e5fcb8aac7f07a87c8d2990
Bakame1 <marko@danet.one>
2026-02-03 16:11:18 +0100

    start part3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import { TaskList } from './components/task-list/task-list'; // Import auto
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: TaskList }, // Chemin vide = page d'accueil
+];
+
